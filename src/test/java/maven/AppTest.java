7bdb4affd7a31bc9827b612923347f66585d8f62
sbur4 <serhii.burch@gmail.com>
2021-07-16 15:39:48 +0300

    JUnit maven

---
+++ src/test/java/maven/AppTest.java
@@ -0,0 +1,28 @@
+package maven;
+
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+public class AppTest {
+
+    @Test
+    @DisplayName("My first JUnit test")
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
+//@ParameterizedTest
+//@RepeatedTest
+//@TestFactory
+//@TestTemplate
+//@TestMethodOrder
+//@TestInstance
+//@DisplayNameGeneration
+//@Nested
+//@Tag
+//@Disabled
+//@Timeout
+//@ExtendWith
+//@RegisterExtension
+//@TempDir
